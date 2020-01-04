c46bceb9612ab9a8525f89abb9485bc989fd628d
michellai <michellai@gmail.com>
2020-01-03 19:53:29 -0800

    fixed merge comflicts

---
+++ app/javascript/packs/test.jsx
@@ -0,0 +1,8 @@
+console.log('hello from test pack');
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
