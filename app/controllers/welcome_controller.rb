4d6a2b9a9b35b3ece5157d115aad621279e67b77
noe stauffert <noe.stauffert@gmail.com>
2020-01-03 19:42:40 -0800

    get restaurant list from api call instead of html

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,2 @@
 class WelcomeController < ApplicationController
-  def home
-    @restaurants = Restaurant.all.as_json(only: [:id, :name])
-  end
 end
--- app/javascript/packs/restaurant_search.jsx
+++ app/javascript/packs/restaurant_search.jsx
@@ -4,15 +4,12 @@ import ReactDOM from 'react-dom'
 import Autocomplete from "./restaurant_autocomplete";
 
 document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('restaurants-data')
-  const data = JSON.parse(node.getAttribute('data'))
-
   const submitNode = document.getElementById('search-submit')
   submitNode.disabled = true
   submitNode.style.cursor = 'not-allowed'
 
   ReactDOM.render(
-    <Autocomplete suggestions={data} />,
+    <Autocomplete />,
     document.getElementById('search-input-field')
   )
 })
